d8f50f89d021e1159793051e1e96c82d7cbafb7a
RIMONJIT-SAIKIA <rimonjit147@gmail.com>
2024-08-04 01:09:40 +0530

    added scrollbar

--- project.js
+++ project.js
@@ -1,13 +1,8 @@
-// document.addEventListener('DOMContentLoaded', (event) => {
-//     let artist_left = document.getElementById('artist_left');
-//     let artist_right = document.getElementById('artist_right');
-//     let ARTISTS = document.getElementsByClassName('ARTISTS')[0];
-
-//     artist_right.addEventListener('click', () => {
-//         ARTISTS.scrollLeft += 100;
-//     })
-
-//     artist_left.addEventListener('click', () => {
-//         ARTISTS.scrollLeft -= 100;
-//     })
-// });
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    if(menu.style.display === 'block'){
+        menu.style.display = 'none';
+    }else{
+        menu.style.display = 'block';
+    }
+}
