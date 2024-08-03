dabe70842c72210e065d527b4aae33e2879d7e43
RIMONJIT-SAIKIA <rimonjit147@gmail.com>
2024-08-03 22:12:56 +0530

    made some changes

---
+++ project.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     let artist_left = document.getElementById('artist_left');
+//     let artist_right = document.getElementById('artist_right');
+//     let ARTISTS = document.getElementsByClassName('ARTISTS')[0];
+
+//     artist_right.addEventListener('click', () => {
+//         ARTISTS.scrollLeft += 100;
+//     })
+
+//     artist_left.addEventListener('click', () => {
+//         ARTISTS.scrollLeft -= 100;
+//     })
+// });
