211c454efb540f0afb37a6400c54bec6f5e5f06b
RIMONJIT-SAIKIA <rimonjit147@gmail.com>
2025-07-01 04:31:22 +0530

    new features

--- project.js
+++ project.js
@@ -6,3 +6,34 @@ function toggleMenu() {
         menu.style.display = 'block';
     }
 }
+
+let buttons = document.querySelectorAll(".modebtn");
+let body = document.querySelector("body");
+
+function clearThemes() {
+  body.classList.remove("dark", "default","light", "bluish", "neon");
+}
+document.body.classList.add(localStorage.getItem('theme'));
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    clearThemes();
+    let mode = button.getAttribute("data-mode");
+    body.classList.add(mode);
+    localStorage.setItem('theme', mode );
+    console.log("Theme changed to:", mode); // Debug check
+  });
+});
+const tabs = document.querySelectorAll(".tab");
+  const contents = document.querySelectorAll(".tab-content");
+
+  tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+      // Remove active class from all tabs and contents
+      tabs.forEach(t => t.classList.remove("active"));
+      contents.forEach(c => c.classList.remove("active"));
+
+      // Add active class to the clicked tab and its content
+      tab.classList.add("active");
+      document.getElementById(tab.getAttribute("data-tab")).classList.add("active");
+    });
+  });
